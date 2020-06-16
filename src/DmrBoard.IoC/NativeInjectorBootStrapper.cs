96524ecd7b2729a3c61f54cb3c1c739f2f55839e
GLB90061476 <GLB90061476@GB90061476N1>
2020-06-16 22:55:55 +0300

    xunit test

--- src/DmrBoard.IoC/NativeInjectorBootStrapper.cs
+++ src/DmrBoard.IoC/NativeInjectorBootStrapper.cs
@@ -18,7 +18,7 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace DmrBoard.IoC
 {
-    public class NativeInjectorBootStrapper
+    public static class NativeInjectorBootStrapper
     {
 
         public static void RegisterServices(IServiceCollection services)
---
+++ src/test/DmrBoard.Application.Tests/OrganizationAppServiceTest.cs
@@ -0,0 +1,59 @@
+﻿using AutoMapper;
+using DmrBoard.Application.Mapper;
+using DmrBoard.Application.Organizations;
+using DmrBoard.Application.Organizations.Dto;
+using DmrBoard.Core.Bus;
+using DmrBoard.Core.Domain.Interfaces;
+using DmrBoard.Core.Interfaces;
+using DmrBoard.Core.Notifications;
+using DmrBoard.Core.Organizations;
+using DmrBoard.IoC;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace DmrBoard.Application.Tests
+{
+    public class OrganizationAppServiceTest
+    {
+        private readonly IOrganizationAppService _organizationAppService;
+
+        private List<Organization> organizations;
+
+        public OrganizationAppServiceTest()
+        {
+            var mockMediatorHandler = new Mock<IMediatorHandler>();
+            var mockCurrentUserService = new Mock<ICurrentUserService>();
+            var mockOrganizationRepo = new Mock<IRepository<Organization, Guid>>();
+
+            //auto mapper configuration
+            var _mockMapper = new MapperConfiguration(cfg =>
+                {
+                    cfg.AddProfile(new CustomDtoMapper());
+                });
+            var mapper = _mockMapper.CreateMapper();
+
+            organizations = new List<Organization>
+            {
+                new Organization{  Name="1. Organizasyon",Id=Guid.NewGuid()},
+                new Organization { Name="2. Organizasyon",Id=Guid.NewGuid()}
+            };
+            mockOrganizationRepo.Setup(s => s.GetAll()).Returns(organizations);
+
+
+            _organizationAppService = new OrganizationAppService(mockMediatorHandler.Object, mapper,
+mockCurrentUserService.Object, mockOrganizationRepo.Object);
+        }
+        [Fact]
+        public void Should_All_Organizations()
+        {
+            var data = _organizationAppService.GetAll().ToList();
+            Assert.Equal(2, data.Count);
+        }
+
+    }
+}
